84bd10ed6f8db582215c9fa97149993d0a03c793
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-03-29 20:43:48 +0100

    - WIP integrate file upload

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -1,12 +1,13 @@
-namespace asciify;
+namespace asciify.Pages;
 
+using Microsoft.AspNetCore.Components.Forms;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using StaticDust;
 
 public sealed partial class Index
 {
-  List<AsciiPageSize> sizes = new List<AsciiPageSize>
+  private readonly List<AsciiPageSize> _sizes = new List<AsciiPageSize>
   {
     new AsciiPageSize("micro", 320, 240),
     new AsciiPageSize("tiny", 480, 360),
@@ -16,13 +17,14 @@ public sealed partial class Index
     new AsciiPageSize("extra large", 1280, 1024),
     new AsciiPageSize("super large", 1600, 1200)
   };
-  
-  private void CreateAsciiArt(Image<Rgba32> mImageSelectorImage, AsciiPageSize selectedPageSize)
+  private string _rendered { get; set; }
+
+  private void CreateAsciiArt(Image<Rgba32> img, AsciiPageSize selectedPageSize)
   {
     // calculate block size
     const int FudgeFactor = 5;
-    var blockWidth = mImageSelectorImage.Width / selectedPageSize.Width;
-    var blockHeight = mImageSelectorImage.Height / selectedPageSize.Height;
+    var blockWidth = img.Width / selectedPageSize.Width;
+    var blockHeight = img.Height / selectedPageSize.Height;
     var blockSize = FudgeFactor * Math.Max(blockWidth, blockHeight);
     blockSize = Math.Max(1, blockSize);
     
@@ -30,10 +32,15 @@ public sealed partial class Index
     var filename = Path.GetTempPath();
     using (var fs = new StreamWriter(filename))
     {
-      AsciiArt.ConvertImage(mImageSelectorImage, fs, (int)blockSize, 5, false, true);
+      AsciiArt.ConvertImage(img, fs, (int)blockSize, 5, false, true);
     }
 
     // save to file
     //File.WriteAllText(filename, browserStr);
+  }
+
+  private void LoadFile(InputFileChangeEventArgs e)
+  {
+    _rendered = $"<b>{e.File.Name}</b>";
   }
 }
