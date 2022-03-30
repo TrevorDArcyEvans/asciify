f4cd4a0e50b282857b8fe72b7f4395e87e8e5867
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2022-03-30 16:31:49 +0100

    - CLI test harness

---
+++ asciify.ui.cli/Options.cs
@@ -0,0 +1,15 @@
+﻿namespace asciify.ui.cli;
+
+using CommandLine;
+
+internal sealed class Options
+{
+  [Value(index: 0, Required = true, HelpText = "Path to image file")]
+  public string ImageFilePath { get; set; }
+
+  [Option('s', "Size", Required = false, Default = "medium", HelpText = "Size of output image")]
+  public string Size { get; set; }
+
+  [Option('o', "Output", Required = false, Default = "index.html", HelpText = "Path to output file")]
+  public string OutputFilePath { get; set; }
+}
---
+++ asciify.ui.cli/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace asciify.ui.cli;
+
+using CommandLine;
+using core;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using StaticDust;
+
+internal static class Program
+{
+  public static async Task Main(string[] args)
+  {
+    var result = await Parser.Default.ParseArguments<Options>(args)
+      .WithParsedAsync(Run);
+    await result.WithNotParsedAsync(HandleParseError);
+  }
+
+  private static async Task Run(Options opt)
+  {
+    var img = await Image.LoadAsync<Rgba32>(opt.ImageFilePath);
+    var size = AsciiPageSize.DefaultSizes.Single(x => x.Name == opt.Size);
+    var html = CreateAsciiArt(img, size);
+    await File.WriteAllTextAsync(opt.OutputFilePath, html);
+  }
+
+  private static string CreateAsciiArt(Image<Rgba32> img, AsciiPageSize selectedPageSize)
+  {
+    // calculate block size
+    const int FudgeFactor = 5;
+    var blockWidth = img.Width / selectedPageSize.Width;
+    var blockHeight = img.Height / selectedPageSize.Height;
+    var blockSize = FudgeFactor * Math.Max(blockWidth, blockHeight);
+    blockSize = Math.Max(1, blockSize);
+    
+    // convert to ascii
+    return AsciiArt.ConvertImage(img, blockSize, 5, false, true);
+  }
+
+  private static Task HandleParseError(IEnumerable<Error> errs)
+  {
+    if (errs.IsVersion())
+    {
+      Console.WriteLine("Version Request");
+      return Task.CompletedTask;
+    }
+
+    if (errs.IsHelp())
+    {
+      Console.WriteLine("Help Request");
+      return Task.CompletedTask;
+      ;
+    }
+
+    Console.WriteLine("Parser Fail");
+    return Task.CompletedTask;
+  }
+}
